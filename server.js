ec84f83a4798c64a5e6a77267e679bad332cc2a9
mahawarKhushi <21uec155@lnmiit.ac.in>
2025-08-29 22:17:12 +0530

    K

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ app.get('/', (req, res) => res.send('API running'));
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  //
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
